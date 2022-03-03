982d43d02f434558bdc2476065038c0026355c80
GIRISHVBHAGWAT <bhagwatgirish337@gmail.com>
2022-03-03 22:49:18 +0530

    text_document_added_faltu

--- save.sh
+++ save.sh
@@ -1,10 +1,11 @@
-git config --global user.name GIRISHVBHAGWAT
-git config --global user.email bhagwatgirish337@gmail.com
-git init
 git add -A
+
 read user_commit
+
 git commit -a -m $user_commit
 
-git remote add padyam https://github.com/GIRISHVBHAGWAT/padyam.git
-git push -u padyam main
+git push padyam
+
 read -p "Enter to exit"
+
+
---
+++ setup.sh
@@ -0,0 +1,8 @@
+git config --global user.name GIRISHVBHAGWAT
+git config --global user.email bhagwatgirish337@gmail.com
+git init
+git add -A
+read user_commit
+git commit -a -m $user_commit
+git remote add padyam https://github.com/GIRISHVBHAGWAT/padyam.git
+read -p "Enter to exit"
